50203b4a79678d7280a6f90d6940a5ff81b980ba
CorinneWilson <cowilson@ucsd.edu>
2024-03-07 16:24:37 -0800

    update grader

--- grading-area/TestListExamples.java
+++ grading-area/TestListExamples.java
@@ -3,13 +3,15 @@ import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
 
-class IsMoon implements StringChecker {
+class IsA implements StringChecker {
   public boolean checkString(String s) {
-    return s.equalsIgnoreCase("moon");
+    return s.equalsIgnoreCase("a");
   }
 }
 
 public class TestListExamples {
+  
+
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
@@ -18,4 +20,39 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testMerge() {
+    List<String> left = Arrays.asList("a", "a", "a");
+    List<String> right = Arrays.asList("a", "d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "a", "a", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeEmpty() {
+    List<String> left = Arrays.asList();
+    List<String> right = Arrays.asList();
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter1() {
+    List<String> list = Arrays.asList("a", "b", "a");
+    List<String> filtered = ListExamples.filter(list,  new IsA());
+    List<String> expected = Arrays.asList("a", "a");
+    assertEquals(expected, filtered);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter2() {
+    List<String> list = Arrays.asList("c", "b");
+    List<String> filtered = ListExamples.filter(list,  new IsA());
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
 }
