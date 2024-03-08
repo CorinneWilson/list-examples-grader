884e12be3803ed3024adff746fb7e7bd51676941
CorinneWilson <cowilson@ucsd.edu>
2024-03-07 17:42:20 -0800

    grade.sh

--- grade.sh
+++ grade.sh
@@ -1,9 +1,9 @@
-CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission 2> ta-output.txt
 
-rm -rf grading-area
+rm -rf grading-a
 
 mkdir grading-area
 
